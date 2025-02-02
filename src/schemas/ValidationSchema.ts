1279f4cb75b7666688d7af7dc0fb931157836b77
Rakesh Biswas <rbiswas01999@gmail.com>
2025-02-02 10:40:31 +0600

    carValidationSchema created

---
+++ src/schemas/ValidationSchema.ts
@@ -0,0 +1,20 @@
+import { z } from 'zod';
+
+const carValidationSchema = z.object({
+    brand: z.string().min(1, 'Brand is required'),
+    model: z.string().min(1, 'Model is required'),
+    year: z.string().min(1, "year is required"),
+    price: z.string().min(1, 'Price is required'),
+    category: z.string().min(1, 'Category is required'),
+    image: z.string()
+        .url('Please enter a valid image URL')
+        .refine(val => !!val, {
+            message: 'Image URL is required',
+        })
+        .optional(),
+    description: z.string().min(1, 'Description is required'),
+    quantity: z.string().min(1, 'Quantity must be at least 1'),
+});
+
+export default carValidationSchema;
+
