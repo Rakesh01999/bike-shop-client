b07bd5f59ada1b4f06ee3731f4764ecf07fa1011
Rakesh Biswas <rbiswas01999@gmail.com>
2025-02-02 14:07:18 +0600

    Adinroute, Userroute added

---
+++ src/routes/UserRoutes.tsx
@@ -0,0 +1,15 @@
+import ChangePassword from "../pages/Dashboard/User/Changepassword";
+import ViewOrder from "../pages/Dashboard/User/ViewOrder";
+
+export const userChildren = [
+  {
+    name: "Change_password",
+    path: "change_password",
+    element: <ChangePassword />,
+  },
+  {
+    name: "View Order",
+    path: "view_order",
+    element: <ViewOrder />,
+  },
+];
