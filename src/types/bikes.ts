4597244427c55bed177a4cf36d7ff6d5a7e88da4
Rakesh Biswas <rbiswas01999@gmail.com>
2025-02-02 09:13:45 +0600

    types>bikes.ts created

---
+++ src/types/bikes.ts
@@ -0,0 +1,27 @@
+export type Bike = {
+    _id: string;
+    brand: string;
+    model: string;
+    year: number;
+    price: number;
+    category: string;
+    description: string;
+    quantity: number;
+    inStock: boolean;
+    image: string;
+    isDeleted: boolean;
+    createdAt: string;
+    updatedAt: string;
+};
+
+
+export interface Order {
+    email: string;
+    bike: string;
+    quantity: number;
+    totalPrice: number;
+    name: string;
+    phone_number: string;
+    address: string;
+}
+
