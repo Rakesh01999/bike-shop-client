f352c3247bab1ae20072d4dc1b5f39f328d15b94
Rakesh Biswas <rbiswas01999@gmail.com>
2025-02-02 11:04:09 +0600

    auth api added

---
+++ src/redux/features/auth/authApi.ts
@@ -0,0 +1,34 @@
+import { baseApi } from "../../api/baseApi";
+
+const authApi = baseApi.injectEndpoints({
+    endpoints: (builder) => ({
+        login: builder.mutation({
+            query: (userInfo) => ({
+                url: "/login",
+                method: "POST",
+                body: userInfo,
+            }),
+            invalidatesTags: ['reset']
+        }),
+        register: builder.mutation({
+            query: (userInfo) => ({
+                url: "/register",
+                method: "POST",
+                body: userInfo,
+            }),
+            invalidatesTags: ['reset']
+        }),
+        changePassword: builder.mutation({
+            query: (userInfo) => ({
+                url: "/change-password",
+                method: "POST",
+                body: userInfo,
+            }),
+            invalidatesTags: ['reset']
+        }),
+
+    }),
+});
+
+export const { useLoginMutation, useRegisterMutation, useChangePasswordMutation } = authApi;
+
