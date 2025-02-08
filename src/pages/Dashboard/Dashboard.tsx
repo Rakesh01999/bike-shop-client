472bd75417ed219069fbf04854f8b92287e08d84
Rakesh Biswas <rbiswas01999@gmail.com>
2025-02-08 22:12:06 +0600

    Dashboard modified

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,39 +1,37 @@
 import MainLayout from "../../components/layout/MainLayout";
 import { useCurrentUser } from "../../redux/features/auth/authSlice";
 import { useAppSelector } from "../../redux/hooks";
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import Navbars from "../Navbar/Navbars";
 
 type UserType = {
-    email: string;
-    // role: "admin" | "faculty" | "student" | "user";
-    role: "admin" | "faculty" | "student" | "user" | "customer";
-    exp: number;
-    iat: number;
-  };
+  email: string;
+  // role: "admin" | "faculty" | "student" | "user" | "customer";
+  role: "admin" | "user" | "customer";
+  exp: number;
+  iat: number;
+};
 
 const Dashboard = () => {
+  const user = useAppSelector(useCurrentUser) as UserType | null;
 
-     const user = useAppSelector(useCurrentUser) as UserType | null;
-    
-      const role = user?.role;
+  const role = user?.role;
 
-    const navigate = useNavigate();
-    useEffect(() => {
-        if(role === 'admin'){
-            // navigate('/dashboard/get_car');
-            navigate('/dashboard/get_bike');
-        }
-        else{
-            navigate('/dashboard/change_password');
-        }
-    }, [navigate,role]);
-    return (
-        <Navbars>
-            <MainLayout/>
-        </Navbars>
-    );
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (role === "admin") {
+      // navigate('/dashboard/get_car');
+      navigate("/dashboard/get_bike");
+    } else {
+      navigate("/dashboard/change_password");
+    }
+  }, [navigate, role]);
+  return (
+    <Navbars>
+      <MainLayout />
+    </Navbars>
+  );
 };
 
 export default Dashboard;
